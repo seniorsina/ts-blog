6a5ae2fb2b5c98d918de5c3b50fbaa3a61dd9d9f
senior sina <senior.sina@gmail.com>
2022-09-10 00:14:40 +0430

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="content">
+        <Home></Home>
+      </div>
+    </div>
+  );
 }
 
 export default App;
